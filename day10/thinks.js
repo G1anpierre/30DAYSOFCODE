e2c91dae45c43797d0f005a9073b7ef0716305b3
Mayta Trigozo, G <g.mayta.trigozo@accenture.com>
2019-09-27 00:08:04 +0200

    refactoring gianpierre code

--- day10/thinks.js
+++ day10/thinks.js
@@ -1,11 +1,5 @@
 
-const imagen = document.querySelector('img');
-const name = document.querySelector('#name');
-const lastName = document.querySelector('#lastName');
-const other = document.querySelector('#otherInfo');
-const email = document.querySelector('#email');
-const city = document.querySelector('#city');
-
+const cardElement = document.querySelector('.cardEl');
 const but = document.querySelector('button');
 var url = 'https://randomuser.me/api/'
 
@@ -18,33 +12,37 @@ but.addEventListener('click', function(){
 function fetchCall(){
     fetch(url)
     .then(handleErrors)
-    .then(parseJSON)
+    .then(response => response.json())
     .then(updateProfile)
     .catch(alertError);
+}
 
-    function handleErrors(request){
-        if(request.status !== 200){
-            throw Error('A SUPER FATAL ERROR :)');
-        }
-        return request;
+function handleErrors(request){
+    if(request.status !== 200){
+        throw Error('A SUPER FATAL ERROR :)');
     }
+    return request;
+}
 
-    function parseJSON(response){
-        let data = response.json();
-        return data;
-    }
+function updateProfile(dataReady){
+    // Data destructuring
+    const { name, login, email, location, picture } = dataReady.results[0];
+    // Template literal
+    cardElement.innerHTML = `
+        <img src="${picture.medium}" alt="profile Picture">
+        <div class="data-info">
+            <div>
+                <h4>${name.first} ${name.last}</h4>
+                <p class="otherInfo">${login.username}</p>
+            </div>
+            <div>
+                <p>Email: ${email}</p>
+                <p>City: ${location.city}</p>
+            </div>
+        </div>
+    `;
+}
 
-    function updateProfile(dataReady){
-        imagen.setAttribute("src", dataReady.results[0].picture.medium);
-        name.textContent = dataReady.results[0].name.first;
-        lastName.textContent = dataReady.results[0].name.last;
-        other.textContent = dataReady.results[0].login.username;
-        email.textContent = dataReady.results[0].email;
-        city.textContent = dataReady.results[0].location.city;
-    }
-
-    function alertError(responseError){
-        alert('An error has occur ' + responseError);
-    }
-
+function alertError(responseError){
+    alert('An error has occur ' + responseError);
 }
